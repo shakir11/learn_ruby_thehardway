813f4c0ebf5eb11140e060700e1583a8b4f3ba1e
shakir11 <shilgam@mail.ru>
2017-04-21 00:57:12 +0300

    Add ex39 hash

---
+++ ex39.rb
@@ -0,0 +1,28 @@
+things = ['a', 'b', 'c', 'd']
+# puts things
+# puts
+# puts things[1]
+# puts
+
+# things[1] = 'z'
+# puts things[1]
+
+
+# Hashes
+stuff = {'name' => 'Zed', 'age' => 39, 'height' => 78}
+# puts stuff
+# puts stuff['name']
+stuff['sity'] = 'San Francisco'
+# puts stuff['sity']
+# puts stuff
+# stuff.delete('sity')
+# puts stuff
+# puts stuff['sity']
+
+states = {
+	'OR' => 'Oregon',
+	'FL'=> 'Florida', 
+	'NY' => 'New York' 
+}
+puts states ['OR']
+puts "Lived in #{states['OR']}"
