ad86e194e9fdfde0764216e30e6c0c3a3c3e88e2
shakir11 <shilgam@mail.ru>
2017-04-25 00:04:27 +0300

    Added ex32: arrays

--- ex32.rb
+++ ex32.rb
@@ -2,26 +2,42 @@ the_count = [1, 2, 3, 4, 5]
 fruits = ['apples', 'oranges', 'pears', 'apricots']
 change = [1, 'pennies', 2, 'dimes', 3, 'quarters']
 
-# Tradititonal style
-for number in the_count
-	puts "This is count #{number}"
-end
+## Tradititonal style
+# for number in the_count
+# 	puts "This is count #{number}"
+# end
 
-# Ruby for-loops:
-fruits.each do |fruit|
-	puts "A fruit of type: #{fruit}"
-end
+## Ruby for-loops:
+# fruits.each do |fruit|
+# 	puts "A fruit of type: #{fruit}"
+# end
 
-change.each {|i| puts "I got #{i}" }
+# change.each {|i| puts "I got #{i}" }
 
-#we can also build lists, first start with an empty one
-elements = []
 
-# Using the range operator:
-(0..5).each do |i|
-	puts "adding #{i} to the list."
-	elements.push(i)
-end
+## Using the range operator:
+# (0..5).each do |i|
+# 	puts "adding #{i} to the list."
+# 	elements.push(i)
+# end
 
-# print all elem
-elements.each {|i| puts "Element was: #{i}"}
+# # print all elem
+# elements.each {|i| puts "Element was: #{i}"}
+
+
+
+# array = Array.new(2) {Array.new(4)}
+# (0..1).each do |x|
+# 	(0..3).each do |y|
+# 		count += 1
+# 		array 
+
+arr = [1, 2, 3, 4, 5]
+arr.each { |a| print a -= 10,  "   " }
+
+
+puts
+
+count = -1
+ar = Array.new(4) { Array.new(4){ count += 1 } }
+puts ar
